7d698fb42626d72414664a3696ca04757c5e5e62
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2025-01-06 04:35:54 +0100

    added volume sliders to options

--- Enemies/skeleton_minion.gd
+++ Enemies/skeleton_minion.gd
@@ -1,7 +1,9 @@
 extends Enemy
 
+class_name SkeletonMinion
+
 func attack(target):
 	attack_component._rotate_to_target(target)
-	attack_component._shoot(target)
+	attack_component._melee_attack(target)
 	animation_player.speed_scale = 5
 	animation_player.play("1H_Melee_Attack_Chop")
---
+++ Enemies/skeleton_warrior.gd
@@ -0,0 +1,9 @@
+extends Enemy
+
+class_name SkeletonWarrior
+
+func attack(target):
+	attack_component._rotate_to_target(target)
+	attack_component._melee_attack(target)
+	animation_player.speed_scale = 5
+	animation_player.play("2H_Melee_Attack_Chop")
