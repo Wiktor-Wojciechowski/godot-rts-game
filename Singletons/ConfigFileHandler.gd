ff172d8de2962963b65af675ca21ed456370538c
Wiktor <109503651+Wiktor-Wojciechowski@users.noreply.github.com>
2025-01-24 11:27:28 +0100

    implemented saving and loading audio settings

--- Singletons/ConfigFileHandler.gd
+++ Singletons/ConfigFileHandler.gd
@@ -2,10 +2,24 @@ extends Node
 
 
 var config = ConfigFile.new()
-var SETTINGS_FILE_PATH = OS.get_executable_path() + "/settings.ini"
+var SETTINGS_FILE_PATH = "user://settings.ini"
 
 func _ready() -> void:
-	print(SETTINGS_FILE_PATH)
 	if !FileAccess.file_exists(SETTINGS_FILE_PATH):
+		print("creating file")
 		config.set_value("audio", "music_volume", 0.5)
 		config.set_value("audio", "sfx_volume", 0.5)
+		
+		config.save(SETTINGS_FILE_PATH)
+	else:
+		config.load(SETTINGS_FILE_PATH)
+
+func save_audio_setting(key:String, value):
+	config.set_value("audio", key, value)
+	config.save(SETTINGS_FILE_PATH)
+
+func load_audio_settings():
+	var audio_settings = {}
+	for key in config.get_section_keys("audio"):
+		audio_settings[key] = config.get_value("audio", key)
+	return audio_settings
--- UI/volume_slider.gd
+++ UI/volume_slider.gd
@@ -5,6 +5,13 @@ extends HSlider
 var bus_index: int
 
 func _ready() -> void:
+	var audio_settings = ConfigFileHandler.load_audio_settings()
+	match bus_name:
+		"SFX":
+			print("audios: ", audio_settings)
+			value = audio_settings.sfx_volume
+		"Music":
+			value = audio_settings.music_volume
 	bus_index = AudioServer.get_bus_index(bus_name)
 	value_changed.connect(_on_value_changed)
 	#value = db_to_linear(AudioServer.get_bus_volume_db(bus_index))
@@ -12,3 +19,8 @@ func _ready() -> void:
 
 func _on_value_changed(value: float) -> void:
 	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+	match bus_name:
+		"SFX":
+			ConfigFileHandler.save_audio_setting("sfx_volume", value)
+		"Music":
+			ConfigFileHandler.save_audio_setting("music_volume", value)
